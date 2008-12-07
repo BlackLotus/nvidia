c3216e56532db5ad8c0de34535d3b9e0379e8124
jensp <jens@jenux.homelinux.org>
2008-12-07 17:39:55 +0100

    updated the livecd functions to my current working version

--- code/livecd-functions.sh
+++ code/livecd-functions.sh
@@ -1,4 +1,4 @@
-alias makesqfs="cd /pub/livecd/source && time mksquashfs . ../target/archlive.sqfs -ef ../exclude -wildcards -noappend -sort ../load.order.new"
+alias makesqfs="cd /pub/livecd/source && sed -i "s/localhost/hansmaulwurf.is-a-geek.org/" etc/pacman.conf && time mksquashfs . ../target/archlive.sqfs -ef ../exclude -wildcards -noappend -sort ../load.order.new && sed -i "s/hansmaulwurf.is-a-geek.org/localhost/" etc/pacman.conf"
 makelivecd() {
 	mount-chroot
 	chroot source /bin/bash --login <<CHROOTED
@@ -9,14 +9,16 @@ CHROOTED
 	time mkisofs -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -c boot.catalog -o /pub/livecd/livecd-$(date +%F-%H-%M).iso -x files /pub/livecd/target/
 }
 makeliveusb() {
+	# this function requires the archiso package to be installed
 	mount-chroot
         chroot source /bin/bash --login <<CHROOTED
-        mkinitcpio -k $(ls /lib/modules) -v -g /boot/initramfs -c /etc/mkinitcpio-usb.conf
+        mkinitcpio -k $(ls /pub/livecd/source/lib/modules) -v -g /boot/initramfs -c /etc/mkinitcpio-usb.conf
 CHROOTED
 	umount-chroot
         cp -R /pub/livecd/source/boot/* /pub/livecd/target/boot/
 	sed -i 's/(cd)/(hd0,0)/g' target/boot/grub/*.lst	
-	mkusb /pub/livecd/target/ /pub/livecd/liveusb-$(date +%F-%H-%M).img
+	modprobe loop
+	mkusbimg /pub/livecd/target/ /pub/livecd/liveusb-$(date +%F-%H-%M).img
 }	
 mount-chroot() {
 	cd /pub/livecd
@@ -35,3 +37,4 @@ umount-chroot() {
 		return 1
 	fi
 }
+
