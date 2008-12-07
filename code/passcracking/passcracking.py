826bfcffcabc8d56ca3b66d03d0a1d14412558f6
BlackLotus <maxmusterm@gmail.com>
2008-12-07 21:44:20 +0100

    Love to make small commits.Now made passcracking.py as need as Shino_ wants it

--- code/passcracking/passcracking.py
+++ code/passcracking/passcracking.py
@@ -2,9 +2,9 @@ import hashfind
 import sys
 
 for hashid in range(1,len(sys.argv)):
-   hash=sys.argv[hashid]
-   (the,password)=hashfind.crack(hash)
-   if the != hash:
-      print hash+' not found'
-   else:
-      print hash+'='+password
+	hash=sys.argv[hashid]
+	(the,password)=hashfind.crack(hash)
+	if the != hash:
+		print hash+' not found'
+	else:
+		print hash+'='+password
