9905716e494d05aa921c15987c6dab3a05dfb569
jensp <jens@jenux.homelinux.org>
2008-12-19 18:49:04 +0100

    lots of improvements on my little package checking tool

--- code/printpkgdetails.sh
+++ code/printpkgdetails.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 PKGBUILDSDIR="/home/jens/build/livecd/pkgbuilds"
 getarchver() {
+pkgver=0
+pkgrel=0
 eval $(curl -s "http://repos.archlinux.org/viewvc.cgi/"${pkgname}"/trunk/PKGBUILD?view=co" |egrep '(^pkgver|^pkgrel)')
+_archpkgver=$pkgver
+_archpkgrel=$pkgrel
 }
-for x in $(find "${PKGBUILDSDIR}" -maxdepth 3 -name PKGBUILD)
-do
-	source $x
-	echo $pkgname:$pkgver-$pkgrel - $url
-	getarchver $pkgname
-	echo Arch Linux version: $pkgver:$pkgrel
-done
+parseargs() {
+	case $1 in
+		checknew) export checknew=1;;
+		checkold) export checkold=1;;
+		checksame) export checksame=1;;
+		checkall) export checknew=1 && checkold=1 && checksame=1;;
+	esac
+}
+if [ "$1" ]
+then
+	parseargs $1
+	for x in $(find "${PKGBUILDSDIR}" -maxdepth 3 -name PKGBUILD)
+	do
+		source $x
+		_chaoxpkgver=$pkgver
+		_chaoxpkgrel=$pkgrel
+		getarchver $pkgname
+		if [[ $pkgver == 0 && $pkgrel == 0 ]]
+		then
+			echo "$pkgname doesn't exist in arch linux"
+		else
+			case $(vercmp $_archpkgver-$_archpkgrel $_chaoxpkgver-$_chaoxpkgrel) in
+				-1) if [[ $checknew == 1 ]];then echo "$pkgname in chaox newer than in arch linux";fi;;
+				0) if [[ $checksame == 1 ]];then echo "$pkgname in chaox same as in arch linux";fi;;
+				1) if [[ $checkold == 1 ]];then echo "$pkgname in arch linux newer than in chaox, check $url for the current version, the arch linux version is $_archpkgver-$_archpkgrel";fi;;
+			esac
+		fi
+	done
+else
+	echo "One of the following arguments has to be supplied: checkall, checknew, checkold, checksame"
+fi
